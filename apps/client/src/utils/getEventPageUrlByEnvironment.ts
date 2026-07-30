16d1392d3ab70fe0380103e5461999e2d37717e7
이태훈 <rhymint@gmail.com>
2026-07-30 17:38:52 +0900

    chore: move service to todari subdomain

--- apps/client/src/utils/getEventPageUrlByEnvironment.ts
+++ apps/client/src/utils/getEventPageUrlByEnvironment.ts
@@ -3,9 +3,7 @@ import {ROUTER_URLS} from '@constants/routerUrls';
 type EventPageTab = 'home' | 'admin';
 
 const getEventPageUrlByEnvironment = (eventId: string, tab: EventPageTab) => {
-  const isDevelopment = process.env.NODE_ENV === 'development';
-
-  return `https://${isDevelopment ? 'dev.' : ''}haengdong.pro${ROUTER_URLS.event}/${eventId}/${tab}`;
+  return `${window.location.origin}${ROUTER_URLS.event}/${eventId}/${tab}`;
 };
 
 export default getEventPageUrlByEnvironment;
